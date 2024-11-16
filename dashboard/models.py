82c9c934d912831fee01e3ed9589d57784ee4ad4
ALEXANDRU STEFANESCU <alexliviu2004@gmail.com>
2024-11-16 19:37:04 +0200

    added buttons and new model

--- dashboard/models.py
+++ dashboard/models.py
@@ -9,19 +9,33 @@ class QuizClass(models.Model):
     def __str__(self):
         return str(self.name)
 
+
 class Student(models.Model):
-    class_name = models.ForeignKey(QuizClass, related_name='students', on_delete=models.CASCADE)
+    class_name = models.ForeignKey(
+        QuizClass, related_name="students", on_delete=models.CASCADE
+    )
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField()
 
     def __str__(self):
-        return f'{self.first_name} {self.last_name}'
+        return f"{self.first_name} {self.last_name}"
+
 
 class Quiz(models.Model):
-    class_name = models.ForeignKey(QuizClass, related_name='quizes', on_delete=models.CASCADE)
+    class_name = models.ForeignKey(
+        QuizClass, related_name="quizes", on_delete=models.CASCADE
+    )
     name = models.CharField(max_length=100)
     number_of_questions = models.IntegerField()
     questions = models.JSONField()
     answers = models.JSONField()
 
+
+class QuizTry(models.Model):
+    quiz_name = models.ForeignKey(Quiz, related_name="quiz", on_delete=models.CASCADE)
+    student_name = models.ForeignKey(
+        Student, related_name="student", on_delete=models.CASCADE
+    )
+    timestamp = models.DateTimeField()
+    answers = models.JSONField()
